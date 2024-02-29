ee19b65617af1b04c4c2c682594f21d942e06cff
Mohib <mohibullahmohim2020@gmail.com>
2024-02-29 20:01:38 +0600

    map function bujtaci

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
 const arrayNum_1 = [32,23,42,44,43];
 const newArrayNum_1 = [];
 for (const num of arrayNum_1) {
-    // console.log( num );
+    console.log( num );
     const newNum = num * 2;
     newArrayNum_1.push(newNum);
 }
-// console.log('New array :', newArrayNum_1);
+console.log('New array :', newArrayNum_1);
+document.write('<br> New array :', newArrayNum_1);
 
 // simple map function
 
@@ -23,7 +24,26 @@ function functionCalling(arrayNum_1){
     return newArrayNum_1;
 }
 const myNewArray_1 = functionCalling(arrayNum_1);
-console.log(myNewArray_1)
+console.log(myNewArray_1);
+document.write('<br> function call array :', myNewArray_1);
+
+
 // arrow function 
 
-// const arrayNum_2 = () => {}
+const exampleSet = (a,b) => (a*b);
+console.log('example :', exampleSet(3,2));
+
+// const justTest = n => n*2 ;
+
+/* --------------------------------------------------*/
+
+
+const myNewArray_2 = [3,5,6,7,8,9,];
+
+function doubleIt (numb){
+    let number = numb * 2 ;
+    return number ;
+}
+const result = myNewArray_2.map(doubleIt);
+
+console.log('result :', result);
