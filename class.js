e27167c363b857c5ce02e1be40e1cd22fb76b070
Mohib <mohibullahmohim2020@gmail.com>
2024-03-01 22:26:19 +0600

    class method added

---
+++ class.js
@@ -0,0 +1,12 @@
+
+class person{
+    constructor(name, height, proffession){
+        this.perseonName = name;
+        this.personHeight = height;
+        this.personProffession = proffession;
+    }
+}
+
+const mohib = new person ('Mohib', '75 inch', 'Study' );
+
+console.log(mohib)
