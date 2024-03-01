151fe2ea2d13db8df57d4ddaca871f08f6da7b6f
Mohib <mohibullahmohim2020@gmail.com>
2024-03-01 23:23:46 +0600

    class method added

--- class.js
+++ class.js
@@ -4,9 +4,23 @@ class person{
         this.perseonName = name;
         this.personHeight = height;
         this.personProffession = proffession;
-    }
+    } 
+    present() {
+        return 'I have a ' + this.perseonName;
+      }
 }
 
 const mohib = new person ('Mohib', '75 inch', 'Study' );
+console.log(mohib);
 
-console.log(mohib)
+class teacher extends person {
+    constructor(name, height, proffession, favoriteClass){
+        super(name, height, proffession);
+        this.favoriteSubject = favoriteClass;
+    }
+    present() {
+        return 'I have a ' + this.perseonName;
+      }
+}
+const shakib = new teacher ('shakib', '75 inch', 'Study', 'math');
+console.log(shakib);
